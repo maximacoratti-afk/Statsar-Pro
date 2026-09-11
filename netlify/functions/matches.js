283d049f6c80dbbe94333a7914b370c3f34771fb
maximacoratti-afk <maximacoratti@gmail.com>
2026-09-11 17:58:09 -0300

    Delete netlify/functions/matches.js

--- netlify/functions/matches.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function() {
-  const TOKEN = 'b26c58ee9d1b4f8482340c8c4c3cf26b';
-  const from = d => { let x=new Date(); x.setDate(x.getDate()+d); return x.toISOString().split('T')[0]; };
-  const url = `https://api.football-data.org/v4/matches?dateFrom=${from(-2)}&dateTo=${from(2)}`;
-  const r = await fetch(url, {headers:{'X-Auth-Token':TOKEN}});
-  const data = await r.json();
-  return { statusCode: 200, headers:{"Content-Type":"application/json","Access-Control-Allow-Origin":"*"}, body: JSON.stringify(data) };
-}
