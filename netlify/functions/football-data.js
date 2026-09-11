dcc122680636cbc59e5f9ac17be0476547df095a
maximacoratti-afk <maximacoratti@gmail.com>
2026-09-11 18:01:00 -0300

    Update football-data.js

--- netlify/functions/football-data.js
+++ netlify/functions/football-data.js
@@ -1,13 +1,16 @@
 exports.handler = async function() {
   const TOKEN = process.env.FOOTBALL_DATA_TOKEN;
+  if (!TOKEN) {
+    return { statusCode: 500, body: JSON.stringify({ error: "FALTA TOKEN ENV" }) };
+  }
   try {
-    const res = await fetch("https://api.football-data.org/v4/matches?status=FINISHED&limit=80", {
+    const res = await fetch("https://api.football-data.org/v4/matches?limit=76", {
       headers: { "X-Auth-Token": TOKEN }
     });
     const data = await res.json();
     return {
       statusCode: 200,
-      headers: { "Access-Control-Allow-Origin": "*" },
+      headers: { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" },
       body: JSON.stringify(data)
     };
   } catch (e) {
